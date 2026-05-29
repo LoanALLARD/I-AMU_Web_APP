e12002364dea3716060a42be954dd22f76027f32
alexandreJACOB9 <alexandre.jacob36@gmail.com>
2026-05-29 11:19:50 +0200

    Fix path

--- app/src/Controllers/AccueilController.php
+++ app/src/Controllers/AccueilController.php
@@ -1,8 +1,10 @@
 <?php
 namespace Controllers;
 
-class AccueilController{
-    public function index(): void {
-        $this->render('accueil', ['titrePage' => 'Accueil']);
+class AccueilController
+{
+    public function index(): void
+    {
+        require dirname(__DIR__) . '/Views/Page/accueilView.php';
     }
 }
