386152cca8d2c0954fd3e25d25030afd92bb4b95
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amu.fr>
2026-05-30 21:54:26 +0200

    fix : remove one var_dump

--- app/src/Domain/OllamaAdaptater.php
+++ app/src/Domain/OllamaAdaptater.php
@@ -27,7 +27,6 @@ class OllamaAdaptater implements LlmAdaptaterInterface {
             curl_setopt($ch, CURLOPT_URL,"http://i-amu_web_app-ollama2-1:11434/api/generate");
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-            var_dump($ch);
             $response = curl_exec($ch);
 
             return $response;
