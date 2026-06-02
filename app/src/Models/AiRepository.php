1ddb980bb36e4e339bfd3312c56943a2275875b2
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amu.fr>
2026-06-02 16:00:29 +0200

    fix recover AiRepository

---
+++ app/src/Models/AiRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Models;
+
+use PDO;
+
+/*
+ * This class use PDO to recover 
+ * all data about AI in the database
+*/
+
+class AiRepository{
+
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo){
+        $this->pdo = $pdo;
+    }
+
+    public function getModelByName(string $name_AI){
+        $query = $this->pdo->prepare('
+        SELECT * FROM models where name = :name
+        ');
+
+        $query->execute(['name' => $name_AI]);
+
+        $result = $query->fetch();
+
+        if ($result === false) {
+            return null;
+        }
+
+        return $result;
+    }
+}
